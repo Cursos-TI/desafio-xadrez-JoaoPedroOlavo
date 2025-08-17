e301a2d118bfbde24391351736708dc1310245ac
João Pedro Olavo De Matos <98217500+JoaoPedroOlavo@users.noreply.github.com>
2025-08-17 15:43:57 +0000

    Finalizado nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -4,38 +4,55 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void movimentoTorre(int casas){
+    int i = 1;
+    while (i <= casas){
+        printf("Direita\n");
+        i++;
+    }
+}
+
+void movimentoRainha(int casas){
+    int i = 1;
+    do{
+        printf("Esquerda\n");
+        i++;
+    } while (i <= casas);
+}
+
+void movimentoBispo(int casas){
+    for(int h=1; h<=casas; h++){
+        printf("Cima, ");
+        for (int w=1; w<2; w++){
+            printf("Direita\n");
+        }
+    }
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
-    int torre=1, bispo=1, rainha=1;
+    int torre=1, h, w, rainha=1;
     int movimentoCompleto=1;
 
     // Implementação de Movimentação do Bispo
     printf("Movimentação BISPO\n");
-    for(bispo; bispo<=5; bispo++){
-        printf("Cima, Direita\n");
-    }
+    movimentoBispo(5);
 
     // Implementação de Movimentação da Torre
     printf("\nMovimentação TORRE\n");
-    while(torre<=5){
-        printf("Direita\n");
-        torre++;
-    }
+    movimentoTorre(5);
 
     // Implementação de Movimentação da Rainha
     printf("\nMovimentação RAINHA\n");
-    do{
-        printf("Esquerda\n");
-        rainha++;
-    }while(rainha<=8);
+    movimentoRainha(8);
 
     // Nível Aventureiro - Movimentação do Cavalo
     printf("\nMovimentação CAVALO\n");
     while (movimentoCompleto--){
         for (int i=0; i<2; i++){
-            printf("Baixo\n");
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
+        printf("Direita\n");
     }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
